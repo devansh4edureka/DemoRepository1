dbe5913da97ffc59e8b491b2a13073409b977bf6
devansh4edureka <71836019+devansh4edureka@users.noreply.github.com>
2020-09-27 09:47:19 +0530

    Updated Java class Demo2 from remote repo

--- Demo2.java
+++ Demo2.java
@@ -2,5 +2,6 @@ class Demo2 {
 
 public static void main(String args[]){
 	System.out.println("This is Java class Demo2");
+	System.out.println("This is second output line from Java class Demo2");
 }
 }
